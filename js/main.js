d132e170f880c90305762a3a51935122a9e4cfd7
Roberto Enea <rekoderx@gmail.com>
2021-09-27 13:35:03 +0200

    Trying new stuff with map and shit

--- js/main.js
+++ js/main.js
@@ -7,12 +7,37 @@ const _headers = {
   "Content-Tupe": "application/json",
 };
 
+let _freelancers = [];
+
 async function loadUsers() {
   const url = _baseUrl + "/latest";
   const response = await fetch(url, { headers: _headers });
-  //   console.log(response);
   const data = await response.json();
-  console.log(JSON.stringify(data.record));
+  _freelancers = data;
+  // appendCategories(_freelancers)
+
+  let cat = _freelancers.filter((categ) => {
+    return categ.category.title === "cleaning";
+  });
+  console.log(cat);
+
+  function appendCategories(categories) {
+    const cardsContainer = document.querySelector(".cards-container");
+    let html = "";
+    for (const category of categories) {
+      html += `
+        <div class="card area1">
+          <div class="card-image">
+            <img src="${category.category.image}"
+          </div>
+          <h4>${category.category.title}</h4>
+        </div>
+        `;
+    }
+    cardsContainer.innerHTML = html;
+  }
+
+  appendCategories(_freelancers);
 }
 
 loadUsers();
